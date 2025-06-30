d95a7fac82df1c3b3f591b118403f89db1ce3b17
Krish Mehan <krishmehan24@gmail.com>
2025-06-30 16:33:14 +0000

    not getting changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,8 @@ export const metadata: Metadata = {
   description: 'Inceptico: An innovative AI-powered platform to simulate business operations, empowering startups to test, refine, and perfect their strategies. Create a digital twin of your business for predictive analytics, risk assessment, and personalized guidance.',
   icons: {
     icon: '/new-assets/inceptico-logo.png',
+    shortcut: '/new-assets/inceptico-logo.png',
+    apple: '/new-assets/inceptico-logo.png',
   },
 };
 
